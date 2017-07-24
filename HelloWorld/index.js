66c217d79322dde220d9faaa3b3661badc39e7e8
pao <paula.stoica@sap.com>
2017-07-24 11:22:39 +0300

    Hello server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -1,2 +1,34 @@
 /* Hello World! program in Node.js */
-console.log("Hello World!");
+"use strict";
+
+//required module
+var mHttp = require("http");
+
+// variables
+var iPort = 8081;
+var sMessage = "Hello World";
+
+//create Http Server
+var oServer = mHttp.createServer(function(req, res){
+    console.log("Request received");
+
+    // Content Type: text/plain
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+   res.write(sMessage);
+   
+   // Send the response body as "Hello World"
+   res.end('Hello World\n');
+
+   console.log("Response sent!");
+});
+oServer.listen(iPort);
+
+//Console will print the server info
+console.log('Server running at http://127.0.0.1:' + iPort + '/');
+
+
+
+//var sMessage = "Hello World";
+//sMessage = sMessage + "!";
+//console.log(sMessage);
