c43c2eea85f55317dc365557b69edd4f0a5c0523
pao <paula.stoica@sap.com>
2017-07-24 11:40:29 +0300

    return response json from server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -8,14 +8,20 @@ var mHttp = require("http");
 var iPort = 8081;
 var sMessage = "Hello World";
 
+var oResponse = {
+    "returnCode" : "1",
+    "returnMessage" : "Everything's OK",
+    "returnData" : null
+};
+
 //create Http Server
 var oServer = mHttp.createServer(function(req, res){
     console.log("Request received");
 
     // Content Type: text/plain
-   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.writeHead(200, {'Content-Type': 'appplication/json'});
 
-   res.write(sMessage);
+   res.write(JSON.stringify(oResponse));
    
    // Send the response body as "Hello World"
    res.end('Hello World\n');
