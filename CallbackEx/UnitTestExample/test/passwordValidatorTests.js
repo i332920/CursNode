e7a7d6e646a0a136cecf0f86c3ed2f08a9cc5303
pao <paula.stoica@sap.com>
2017-07-25 12:29:06 +0300

    test implemented

--- CallbackEx/UnitTestExample/test/passwordValidatorTests.js
+++ CallbackEx/UnitTestExample/test/passwordValidatorTests.js
@@ -1,7 +1,30 @@
+var chai = require("chai");
+var passwordValidator = require("../modules/passwordValidator");
+
 describe("passwordValidator", function() {
    describe(".validPassword(password)", function() {
        it("it should have minimum 8 characters length", function(){
-           //Test Goes Here
+           var wrongPass = "1234567";
+           var valid = passwordValidator.validPassword(wrongPass);
+
+           //chai check
+           chai.expect(valid).to.be.equal(false);
+
+           var goodPass = "12345678";
+           var valid2 = passwordValidator.validPassword(goodPass);
+           chai.expect(valid2).to.be.equal(true);
+       });
+
+       it("it should not contain 'password' inside", function(){
+           var wrongPass = "password1";
+           var valid = passwordValidator.validPassword(wrongPass);
+
+           //chai check
+           chai.expect(valid).to.be.equal(false);
+
+           var goodPass = "blabla";
+           var valid2 = passwordValidator.validPassword(goodPass);
+           chai.expect(valid2).to.be.equal(true);
        });
    });
 });
