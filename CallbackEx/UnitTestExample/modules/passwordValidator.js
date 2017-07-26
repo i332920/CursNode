783c220019611c03db6924c360e66a1d1786bf91
pao <paula.stoica@sap.com>
2017-07-26 09:19:25 +0300

    testsPasswordValidator

--- CallbackEx/UnitTestExample/modules/passwordValidator.js
+++ CallbackEx/UnitTestExample/modules/passwordValidator.js
@@ -1,3 +1,12 @@
-exports.validPassword = module.exports.validPassword = function (password) {
-return true;
+exports.validPassword  =  module.exports.validPassword  =  function  (password) {
+    if (password.length < 8) {
+        return false;
+    }
+
+    if(password.indexOf("password")>=0){
+    return false;
+    }
+    return true;
+
 };
+
